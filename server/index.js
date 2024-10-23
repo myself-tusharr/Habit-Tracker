30bae1afda256beb585a57c258ad77bf6015b9de
Mr. Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2024-10-23 17:14:30 +0530

    Resolved port issues on server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const api = require('./routes/api')
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 //allowing cross origin access for independant development
 //disable while production
