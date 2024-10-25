caf2c0a8a173fcb9afdb4c9c8a0b8cb659bb42b5
Mr. Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2024-10-25 15:04:18 +0530

    added "Request Received" log

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,12 @@ const port = process.env.PORT || 5001
 app.use('/api', api)
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.use((req, res, next) => {
+    const time = new Date()
+    console.log("Request Recieved on : ", time)
+    next()
+})
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
